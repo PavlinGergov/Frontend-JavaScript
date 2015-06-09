c3d039ac29293ec2587d92d4e918fa41a56d7adf
Pavlin Gergov <pgergov92@abv.bg>
2015-06-09 14:46:40 +0300

    Adding accordion task

--- week4/1-Accordion/app.js
+++ week4/1-Accordion/app.js
@@ -1,5 +1,38 @@
 "use strict";
 
 $(document).ready(function() {
-  $(".accordion").accordion();
+
+  accordion(".accordion");
+
+  function accordion(element){
+    $("a").click(function(x){
+      x.preventDefault();
+    });
+
+    var elements = [];
+    $(element).find("dd").each(function(){
+        var ddId = $(this).attr("class");
+        elements.push(ddId);
+    });
+
+    initialView(elements);
+
+    $("dt").click(function() {
+      var currentDd = $(this).attr("class");
+      $("dd." + currentDd).show();
+      elements.filter(function(item){
+        return item !== currentDd;
+      }).forEach(function(item){
+        $("dd." + item).hide();
+      });
+    });
+
+    function initialView(arr){
+      arr.filter(function(element, index){
+        return index !== 0;
+      }).forEach(function(element, index){
+        $("dd." + element).hide();
+        });
+    }
+  }
 });
